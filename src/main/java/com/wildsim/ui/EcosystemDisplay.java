8d09d3ad38171ab66eb66bb631ae6b4eda4de8ee
andreiOpran <andrei.opran@icloud.com>
2025-05-28 02:08:56 +0300

    randomly mirror carnivores and herbivores

--- src/main/java/com/wildsim/ui/EcosystemDisplay.java
+++ src/main/java/com/wildsim/ui/EcosystemDisplay.java
@@ -16,6 +16,7 @@ public class EcosystemDisplay extends StackPane {
 	private int cellSize  = 96; // size of each cell in pixels
 
 	private Map<Character, Image> textures = new HashMap<>();
+	private java.util.Random random = new java.util.Random();
 
 	public EcosystemDisplay(Ecosystem ecosystem) {
 		this.ecosystem = ecosystem;
@@ -33,9 +34,9 @@ public class EcosystemDisplay extends StackPane {
 
 	private void loadTextures() {
 		textures.put('.', new Image(getClass().getResourceAsStream("/images/empty.png")));
-        textures.put('T', new Image(getClass().getResourceAsStream("/images/tree.png")));
-        textures.put('H', new Image(getClass().getResourceAsStream("/images/herbivore.png")));
-        textures.put('C', new Image(getClass().getResourceAsStream("/images/carnivore.png")));
+		textures.put('T', new Image(getClass().getResourceAsStream("/images/tree.png")));
+		textures.put('H', new Image(getClass().getResourceAsStream("/images/herbivore.png")));
+		textures.put('C', new Image(getClass().getResourceAsStream("/images/carnivore.png")));
 		textures.put('W', new Image(getClass().getResourceAsStream("/images/water_source.png")));
 	}
 
@@ -46,7 +47,25 @@ public class EcosystemDisplay extends StackPane {
 			for (int j = 0; j < ecosystem.getWidth(); j++) {
 				char symbol = matrix[i][j];
 				Image texture = textures.getOrDefault(symbol, textures.get('.'));
-				gc.drawImage(texture, j * cellSize, i * cellSize, cellSize, cellSize);
+
+				// Randomly decide whether to mirror horizontally
+				boolean mirror = random.nextDouble() < 0.25; // 25% chance to mirror
+
+				if (mirror && (symbol == 'H' || symbol == 'C')) { // mirror only for herbivores and carnivores
+					// Save the current state
+					gc.save();
+
+					// Apply transformation: scale by -1 in x direction and translate
+					gc.translate(j * cellSize + cellSize, i * cellSize);
+					gc.scale(-1, 1);
+					gc.drawImage(texture, 0, 0, cellSize, cellSize);
+
+					// Restore the state
+					gc.restore();
+				} else {
+					// Draw normally
+					gc.drawImage(texture, j * cellSize, i * cellSize, cellSize, cellSize);
+				}
 			}
 		}
 	}
